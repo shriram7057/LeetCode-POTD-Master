57460fcac90905867f95e9587febcb16f7e662e6
Mr.Shree ⚡ <172600005+shriram7057@users.noreply.github.com>
2025-12-12 15:14:56 +0530

    [1143] [Time Beats: 53.88%] [Memory Beats: 69.49%] - LeetPush

--- Longest-Common-Subsequence.py
+++ Longest-Common-Subsequence.py
@@ -1,15 +1,16 @@
-class Solution(object):
-    def longestCommonSubsequence(self, text1, text2):
-        m, n = len(text1), len(text2)
-        
-        # DP table with (m+1) x (n+1)
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
-        
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                if text1[i - 1] == text2[j - 1]:
-                    dp[i][j] = dp[i - 1][j - 1] + 1
-                else:
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-        
-        return dp[m][n]
+1class Solution(object):
+2    def longestCommonSubsequence(self, text1, text2):
+3        m, n = len(text1), len(text2)
+4
+5        # DP table of size (m+1) × (n+1)
+6        dp = [[0] * (n + 1) for _ in range(m + 1)]
+7
+8        for i in range(1, m + 1):
+9            for j in range(1, n + 1):
+10                if text1[i - 1] == text2[j - 1]:
+11                    dp[i][j] = dp[i - 1][j - 1] + 1
+12                else:
+13                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+14
+15        return dp[m][n]
+16
