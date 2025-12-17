8aec1ba1dd1f592cdf7132e7ca13761544ee6464
Mr.Shree ⚡ <172600005+shriram7057@users.noreply.github.com>
2025-12-17 17:28:31 +0530

    [120] [Time Beats: 90.53%] [Memory Beats: 63.52%] - LeetPush

--- Triangle.py
+++ Triangle.py
@@ -1,7 +1,9 @@
-class Solution(object):
-    def minimumTotal(self, triangle):
-        dp = triangle[-1][:]
-        for i in range(len(triangle) - 2, -1, -1):
-            for j in range(len(triangle[i])):
-                dp[j] = triangle[i][j] + min(dp[j], dp[j + 1])
-        return dp[0]
+1class Solution:
+2    def minimumTotal(self, triangle):
+3        # Start from second-last row and move upward
+4        for i in range(len(triangle) - 2, -1, -1):
+5            for j in range(len(triangle[i])):
+6                triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1])
+7
+8        return triangle[0][0]
+9
